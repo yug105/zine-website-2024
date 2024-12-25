4ee141683eb740cb828d12d9a159d8131050dd47
yug <yugagarwal105@gmail.com>
2024-12-26 01:04:09 +0530

    commit only team

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -5,7 +5,7 @@ import {
   Carousal
 
 } from "../components/Gallery";
-import CreateNewBlog from "../components/Members/Admin/createblog";
+//import CreateNewBlog from "../components/Members/Admin/createblog";
 import { SecFooter } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
 import { ChatButton } from "../components/Chat";
@@ -21,7 +21,7 @@ const GalleryImages: NextPage = () => {
       <ChatButton />
       <Carousal />
       <Gallery />
-      <CreateNewBlog />
+      {/* <CreateNewBlog /> */}
       <SecFooter />
     </>
   );
